9eee0326155cbbdaa8bad0b4df5356c361b530ad
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-04 22:36:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,5 +5,12 @@ class Anagram
   def initialize(word) 
     @word = word 
   end 
-
-end 
+  
+  def match(word_array) 
+    matches = []
+    word_array.map{|w|
+      matches << w if w.split("").sort == @word.split("").sort
+    }
+    matches
+  end
+end
