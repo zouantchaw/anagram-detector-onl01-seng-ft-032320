011a83e8a9387c38174f10da3d7d9ba063306048
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-04 22:36:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :word 
+  
+  def initialize(word) 
+    @word = word 
+  end 
+  
+  def match(word_array) 
+    matches = []
+    word_array.map{|w|
+      matches << w if w.split("").sort == @word.split("").sort
+    }
+    matches
+  end
+end
